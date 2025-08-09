d9b2542d765e5e62b6159f1895c85220527770f7
haji-mix <hajime20041029@gmail.com>
2025-08-10 05:56:03 +0800

    update

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 const express = require("express");
 const { createProxyMiddleware } = require("http-proxy-middleware");
-const http = require("http");
 
 const app = express();
 
-const primaryProxy = createProxyMiddleware({
-  target: "https://proxy.lkpanio25.workers.dev",
-  changeOrigin: true,
-  pathRewrite: { "^/": "" },
-  onProxyReq: (proxyReq, req) => {
-    const realIp = req.ip;
-    proxyReq.setHeader("X-Forwarded-For", realIp);
-  },
-  selfHandleResponse: true,
-});
-
-app.use(primaryProxy);
+app.use(
+  "/",
+  createProxyMiddleware({
+    target: "https://proxy.lkpanio25.workers.dev",
+    changeOrigin: true,
+    pathRewrite: { "^/": "" },
+    onProxyReq: (proxyReq, req) => {
+      const realIp = req.ip;
+      proxyReq.setHeader("X-Forwarded-For", realIp);
+    },
+    onError: (err, req, res) => {
+      res.status(500).send("Proxy error");
+    },
+  })
+);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
